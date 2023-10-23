40e2f7c80c716234ea5aab4514c4c4aa8dc63ffb
Maksim Movchan <maximusdrlor@gmail.com>
2023-10-23 18:31:57 +0300

    'initial commit'

--- bin/main.dart
+++ bin/main.dart
@@ -1,130 +1,58 @@
-import 'dart:math';
-
-Future<void> main() async {
-  final group1 = [
-    Fencer(name: 'Дима', level: SkillsLevel.profi),
-    Fencer(name: 'Ира', level: SkillsLevel.profi),
-  ];
-  final sportGroup1 = SportGroupClass(group: group1);
-  final trainer1 = FencingTrainer(
-      name: 'Василий Константинопольскович',
-      specialization: Specialization.saber);
-  Sport sport = Sport(trainer: trainer1, group: sportGroup1);
-  sport.organizeFencing();
-  final duel = sport.fencing.organiseDuel();
-  duel.spendDuel();
+void main() {
+  final dino = Animal(weight: 10000, name: "Птеродактиль");
+  dino.out();
+  final dog = Dog(weight: 10, breed: 'чихуахуа', name: "Дмитрий");
+  final cat = Cat(weight: 10, name: "Дмитрий", type: KotKakoy.polosatiy);
+  dog.out();
+  cat.out();
 }
 
-interface class SportGroupClass<T> {
-  SportGroupClass({required this.group});
-  final List<T> group;
-}
-
-class Sport {
-  Sport({required this.trainer, required this.group});
-
-  final FencingTrainer trainer;
-  final SportGroupClass group;
-
-  late final Fencing fencing;
-
-  void organizeFencing() => fencing = Fencing(trainer: trainer, group: group);
-}
-
-class Fencing extends Sport {
-  Fencing({required super.trainer, required super.group});
-
-  Duel organiseDuel() =>
-      Duel(firstOpponent: group.group.first, secondOpponent: group.group.last);
-}
-
-class FencingTrainer {
-  FencingTrainer({
+class Animal {
+  Animal({
+    required this.weight,
     required this.name,
-    required this.specialization,
   });
+  final double weight;
   final String name;
-  final Specialization specialization;
 
-  void outInfo() => print('Имя: $name\nСпециализация: $prepareSpecialization');
-  String prepareSpecialization() {
-    switch (specialization) {
-      case Specialization.bayonet:
-        return "Штыки";
-      case Specialization.rapier:
-        return "Рапира";
-      case Specialization.saber:
-        return "Сабля";
-      case Specialization.swords:
-        return "Меч";
-    }
-  }
+  void out() => print("Вес:$weight, Имя:$name");
 }
 
-class Fencer {
-  Fencer({
-    required this.name,
-    required this.level,
+class Dog extends Animal {
+  final String breed;
+  Dog({
+    required super.weight,
+    required this.breed,
+    required super.name,
   });
-  final String name;
-  final SkillsLevel level;
-  String printInfo() => 'Имя: $name\nУровень подготовки: ${prepareLevel()}';
-  String prepareLevel() {
-    switch (level) {
-      case SkillsLevel.beginner:
-        return "Новичок";
-      case SkillsLevel.enthusiast:
-        return "Любитель";
-      case SkillsLevel.profi:
-        return "Профи";
+
+  @override
+  void out() => print("Вес:$weight, Имя:$name, Порода: $breed");
+}
+
+class Cat extends Animal {
+  final KotKakoy type;
+  Cat({
+    required super.weight,
+    required super.name,
+    required this.type,
+  });
+
+  @override
+  void out() => print("Вес:$weight, Имя:$name, Порода: ${_prepareEnum()}");
+
+  String _prepareEnum() {
+    late final label;
+    switch (type) {
+      case KotKakoy.polosatiy:
+        label = "Полосатый";
+      case KotKakoy.volosatiy:
+        label = "Волосатый";
+      case KotKakoy.lisiy:
+        label = "Лысый";
     }
+    return label;
   }
 }
 
-class Duel {
-  Duel({required this.firstOpponent, required this.secondOpponent});
-  final Fencer firstOpponent;
-  final Fencer secondOpponent;
-  void output() => print(
-      'Первый оппонент: ${firstOpponent.printInfo()}\nВторой оппонент: ${secondOpponent.printInfo()}');
-  void spendDuel() {
-    final chance1 = countChance(fencer: firstOpponent);
-    final chance2 = countChance(fencer: secondOpponent);
-    print("Поединок начался\n");
-    output();
-    late final bool result;
-    if (chance1 == chance2) {
-      result = Random().nextBool();
-    } else {
-      result = chance1 > chance2;
-    }
-    print("Поединок закончился. Победил...\n");
-    print(result ? firstOpponent.printInfo() : secondOpponent.printInfo());
-  }
-
-  static double countChance({required Fencer fencer}) {
-    double chance = 0.0;
-    switch (fencer.level) {
-      case SkillsLevel.profi:
-        chance = 1.0;
-      case SkillsLevel.beginner:
-        chance = 0.3;
-      case SkillsLevel.enthusiast:
-        chance = 0.5;
-    }
-    return chance;
-  }
-}
-
-enum SkillsLevel {
-  beginner,
-  enthusiast,
-  profi,
-}
-
-enum Specialization {
-  rapier,
-  swords,
-  bayonet,
-  saber,
-}
+enum KotKakoy { polosatiy, volosatiy, lisiy }
